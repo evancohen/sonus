391210341d944233a037b1900fef2d1fd4e0b2c3
Evan Cohen <evanbtcohen@gmail.com>
2017-11-14 19:42:49 -0800

    updating examples for new google speech library

--- examples/example.js
+++ examples/example.js
@@ -2,7 +2,9 @@
 
 const ROOT_DIR = __dirname + '/../'
 const Sonus = require(ROOT_DIR + 'index.js')
-const speech = require('@google-cloud/speech')({
+const speech = require('@google-cloud/speech')
+
+const client = new speech.SpeechClient({
   projectId: 'streaming-speech-sample',
   keyFilename: ROOT_DIR + 'keyfile.json'
 })
@@ -11,7 +13,7 @@ const hotwords = [{ file: ROOT_DIR + 'resources/sonus.pmdl', hotword: 'sonus' }]
 const language = "en-US"
 
 //recordProgram can also be 'arecord' which works much better on the Pi and low power devices
-const sonus = Sonus.init({ hotwords, language, recordProgram: "rec" }, speech)
+const sonus = Sonus.init({ hotwords, language, recordProgram: "rec" }, client)
 
 Sonus.start(sonus)
 console.log('Say "' + hotwords[0].hotword + '"...')
@@ -21,6 +23,8 @@ sonus.on('hotword', (index, keyword) => console.log("!" + keyword))
 sonus.on('partial-result', result => console.log("Partial", result))
 
 sonus.on('error', error => console.log('error', error))
+
+sonus.on('timeout', ()=> {console.log('Request timed out')})
 
 sonus.on('final-result', result => {
   console.log("Final", result)
