bd902597125c8ddbc2e3e0aba414926671fcce09
Flynn Buckingham <dev@flynnbuckingham.com>
2018-01-09 04:08:58 -0500

    Fixed integration methods

--- integrations/google.js
+++ integrations/google.js
@@ -1,5 +1,7 @@
 
-module.exports = (speech) => {
+module.exports = speech => {
+	if (typeof speech != 'function') return console.error('invalid instance')
+
 	// do nothing since default is google integration
 	return speech
 }
